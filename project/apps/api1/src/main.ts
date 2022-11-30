c3c06d8019cf2edd72fce133889ceaa102c90b42
justalk <justal.kevin@gmail.com>
2022-11-30 21:10:01 +0100

    [FEATURE] Adding the half open logic

--- project/apps/api1/src/main.ts
+++ project/apps/api1/src/main.ts
@@ -11,27 +11,43 @@ const app = express();
 let counter = 0;
 let date_last_fail = null;
 const CIRCUIT_BREAKER_CLOSED_THRESHOLD = 3;
+const CIRCUIT_BREAKER_HALF_OPEN_THRESHOLD_IN_SECOND = 5;
 
 const SERVER_API2_URL = `${process.env.NX_API_PROTOCOL}://${process.env.NX_API_HOST}:${process.env.NX_API_2_PORT}`;
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+const callApi2 = async (isOpen = true) => {
+  try {
+    const rsl = await axios.get(SERVER_API2_URL);
+    const { data } = rsl;
+    const { message } = data;
+    counter = isOpen ? Math.max(counter - 1, 0) : 0;
+    return message;
+  } catch {
+    counter++;
+    date_last_fail = new Date();
+    return isOpen
+      ? `CIRCUIT OPEN (fail: ${counter})`
+      : `CIRCUIT HALF-OPEN (fail: ${counter})`;
+  }
+};
+
 app.get('/api', async (req, res) => {
   if (counter < CIRCUIT_BREAKER_CLOSED_THRESHOLD) {
-    try {
-      const rsl = await axios.get(SERVER_API2_URL);
-      const { data } = rsl;
-      const { message } = data;
-      return res.send({ message });
-    } catch {
-      counter++;
-      date_last_fail = new Date();
-      return res.send({ message: `CIRCUIT OPEN (fail: ${counter})` });
-    }
+    const message = await callApi2();
+    return res.send({ message });
   } else {
     const now = new Date();
     const diffNowAndLastFail = now.getTime() - date_last_fail.getTime();
-    // Attempt
+    if (
+      diffNowAndLastFail >
+      CIRCUIT_BREAKER_HALF_OPEN_THRESHOLD_IN_SECOND * 1000
+    ) {
+      const message = await callApi2(false);
+      return res.send({ message });
+    }
+
     return res.send({ message: 'CIRCUIT CLOSED' });
   }
 });
